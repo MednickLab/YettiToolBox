ca187cacdaa3108d7e502f0a80b4e71e85a9cd13
Benjamin Yetton <bdyetton@gmail.com>
2017-03-21 13:23:26 -0700

    commented anova file

--- analysis/myAnova.m
+++
@@ -1,7 +0,0 @@
-function myAnova(y,grouping,xLab)
-    if exist('xLab','var')
-        ps = anovan(y,grouping,'model','interaction','varnames',xLab);
-    else
-        ps = anovan(y,grouping,'model','interaction');
-    end
-end
